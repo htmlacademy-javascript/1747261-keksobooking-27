1bcd312584076d68b72f64f71f7063cd18413df7
SofyaIlina <kenny8556@yandex.ru>
2022-10-03 00:50:25 +0500

    добавляет функцию, возвращающую случайное число с плавающей точкой из переданного диапазона включительно
    Будет использоваться для генерации временных географических координат

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const getRandom = function(min,max,precision) {
+  if (min < 0 || max < 0 || precision < 0 || min > max) {
+    return NaN;
+  }
+  if (min === max) {
+    return min.toFixed(precision);
+  }
+  let random = max + 1;
+  while (random > max) {
+    random = Math.random() * (max - min + 1) + min;
+  }
+  return random.toFixed(precision);
+};
+
+getRandom(2,3,2);
